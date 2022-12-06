ea88b8dd875390b4d525a72af72f6005e115ad80
PerfectlyInternal <55606714+PerfectlyInternal@users.noreply.github.com>
2022-12-06 15:33:47 -0500

    Create ArducamStreamTrack.py

---
+++ src/capture/ArducamStreamTrack.py
@@ -0,0 +1,9 @@
+from av import VideoFrame
+from aiortc import MediaStreamTrack
+
+def ArducamStreamTrack(MediaStreamTrack):
+    def __init__(self):
+        pass
+
+    async def recv(self):
+        pass
