4c9ded33a664c1dbef41c582a3817b013a699121
SashaM69420 <alex.n.martynov@gmail.com>
2023-01-04 17:30:37 -0500

    apichanges

--- src/api/rtcapi.py
+++ src/api/rtcapi.py
@@ -1,38 +1,9 @@
 import asyncio
 import logging
 
-from aiortc import RTCPeerConnection, RTCSessionDescription
-
 from httpapi import add_http_route
 
-pcs = set()
+from webrtc_lib import streamer
 
-# a response to a request to open a WebRTC connection
-async def offer(request):
-    params = await request.json()
-    offer = RTCSessionDescription(sdp=params["sdp"], type=params["type"])
-
-    pc = RTCPeerConnection()
-    pcs.add(pc)
-
-    @pc.on("connectionstatechange")
-    async def on_connectionstatechange():
-        print("Connection state is %s" % pc.connectionState)
-        if pc.connectionState == "failed":
-            await pc.close()
-            pcs.discard(pc)
-
-    await pc.setRemoteDescription(offer)
-
-    answer = await pc.createAnswer()
-    await pc.setLocalDescription(answer)
-
-    return web.Response(
-        content_type="application/json",
-        text=json.dumps(
-            {"sdp": pc.localDescription.sdp, "type": pc.localDescription.type}
-        ),
-    )
-
-async def init():
-    add_http_route('open_rtc_connection', offer)
+def init():
+    pass
